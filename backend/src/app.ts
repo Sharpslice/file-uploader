abdceb5f338e541b728253852dc4fc5bea938fda
sharpslice <davidlein1440p@gmail.com>
2025-07-27 23:08:44 -0700

    feat: auth route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,19 @@
 import express, {Request,Response} from 'express';
+import session from 'express-session';
+import passport from 'passport'
+import auth from './routes/auth';
 const app = express();
-
+const authRoute = auth;
 app.use(express.json());
 
-app.get('/',(req,res )=>{
-    res.send('hello world')
-}) 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+
+}))
+
+app.use('/',authRoute);
 
 
 app.listen(3000,()=>{
