9ec04935597dfbebbebe621ecdd6e6c9bfd86f4c
sharpslice <davidlein1440p@gmail.com>
2025-07-28 20:10:58 -0700

    feat: add routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
+
+
+const route = createBrowserRouter([
+    {
+      path:"/",
+      element: <App/>,
+    }
+
+
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router = {route}/>
+  </StrictMode>
 )
