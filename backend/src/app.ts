565aa777201ef0dbf99ff17667b4f9033fa156ce
sharpslice <davidlein1440p@gmail.com>
2025-07-28 21:20:48 -0700

    feat: add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,17 +2,21 @@ import express, {Request,Response} from 'express';
 import session from 'express-session';
 import passport from 'passport'
 import auth from './routes/auth';
+import cors from 'cors';
 const app = express();
 const authRoute = auth;
 app.use(express.json());
-
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true
+}))
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
 
 }))
-
+app.use(passport.session());
 app.use('/',authRoute);
 
 
--- frontend/src/credentials/Signup.tsx
+++ frontend/src/credentials/Signup.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react"
 import InputGroup from "../components/InputGroup"
+import axios from "axios";
 
 function Signup(){
     const [username,setUsername] = useState("");
     const [password,setPassword] = useState("");
 
 
-    const onHandleClick = (e: React.MouseEvent<HTMLElement>) =>{
+    const onHandleClick = async(e: React.MouseEvent<HTMLElement>) =>{
         e.preventDefault()
         console.log(`${username}, ${password}`)
 
+        const response = await axios.post('http://localhost:3000/auth/signup',{username,password},{withCredentials:true})
+        
 
     }
 
