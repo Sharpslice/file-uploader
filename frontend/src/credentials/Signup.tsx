2920e3eeb1174e803e3e38ee807d07af59bc9b85
sharpslice <davidlein1440p@gmail.com>
2025-07-28 20:55:29 -0700

    feat: basic sign up form

--- frontend/src/credentials/Signup.tsx
+++ frontend/src/credentials/Signup.tsx
@@ -3,16 +3,35 @@ import InputGroup from "../components/InputGroup"
 
 function Signup(){
     const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+
+
+    const onHandleClick = (e: React.MouseEvent<HTMLElement>) =>{
+        e.preventDefault()
+        console.log(`${username}, ${password}`)
+
+
+    }
 
     return(
+        
         <form>
         <InputGroup
             title="username"
             type="text"
             value={username}
             setValue={setUsername}
-        
+
         />
+
+        <InputGroup
+            title="password"
+            type="password"
+            value={password}
+            setValue={setPassword}
+
+        />
+        <button onClick={onHandleClick}>sign up</button>
 
 
     </form>
