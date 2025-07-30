0720c5772620c5940140b607c2e20187bb056f73
sharpslice <davidlein1440p@gmail.com>
2025-07-29 20:25:01 -0700

    fix: successful log in will now properly redirect by authenticating now instead of waiting for AuthProvider's useeffect

--- frontend/src/credentials/Login.tsx
+++ frontend/src/credentials/Login.tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import InputGroup from "../components/InputGroup";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../AuthProvider";
 
 function Login(){
     const [username,setUsername] = useState("");
     const [password,setPassword] = useState("");
-
+    const {setIsAuthenticated} = useContext(AuthContext)!;
     const navigate = useNavigate();
     const onHandleClick = async(e: React.MouseEvent<HTMLElement>) =>{
         e.preventDefault()
@@ -14,6 +15,7 @@ function Login(){
         try{
             const response = await axios.post('http://localhost:3000/auth/login',{username,password},{withCredentials:true})
             console.log(response.data.message)
+            setIsAuthenticated(true)
             navigate('/')
         }
         catch(error: unknown){
