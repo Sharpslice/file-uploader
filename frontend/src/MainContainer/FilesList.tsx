c2002577e124574f88d0acb2dd52e855986d8fd2
sharpslice <davidlein1440p@gmail.com>
2025-07-31 20:07:16 -0700

    feat: renders the files from the directory to front end

--- frontend/src/MainContainer/FilesList.tsx
+++ frontend/src/MainContainer/FilesList.tsx
@@ -1,24 +1,30 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 function FilesList(){
 
+
+    const [filesList,setFilesList] = useState([]);
     useEffect(()=>{
         const fetchFiles = async()=>{
             
             const response = await axios.get('http://localhost:3000/files/directory',{withCredentials:true})
             console.log(response.data)
-            
+            setFilesList(response.data.files)
             
             
         }
         fetchFiles();
-    },[])
+    },[filesList])
 
 
 
     return(<>
-        {"files Here"}
+        {filesList.map((file)=>{
+            return (
+                <span>{file}</span>
+            )
+        })}
     
     </>)
 }
