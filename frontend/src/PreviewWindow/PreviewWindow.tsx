3f1fe5f0707d4def5093035f3db0b87162791cf9
sharpslice <davidlein1440p@gmail.com>
2025-08-04 20:25:37 -0700

    feat: clicking on a file now opens a preview window displaying the file image

--- frontend/src/PreviewWindow/PreviewWindow.tsx
+++ frontend/src/PreviewWindow/PreviewWindow.tsx
@@ -1,15 +1,30 @@
 import { useContext } from "react"
 import { PreviewContext } from "../PreviewProvider"
-
+import './PreviewWindow.css'
 function PreviewWindow(){
-    const {fileData} = useContext(PreviewContext)!
+    const {fileData,setFileData} = useContext(PreviewContext)!
  
     if(!fileData) return null
+
+    const onCloseClick=()=>{
+        setFileData(null)
+    }
+
     return(
         <>
-            <div>
-                Hello
-
+            <div className="preview-container">
+                <div className="preview-header">
+                    <button onClick={onCloseClick}>close</button>
+                    <div className="preview-info">
+                        <div>
+                            {fileData.fileName}
+                        </div>
+                        <div>
+                            {fileData.fileType}
+                        </div>
+                    </div>
+                </div>  
+                <img src={fileData.presignedUrl!} alt="" />
 
             </div>
         
